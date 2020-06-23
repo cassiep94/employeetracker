45da2cbeba22d424a81fefa0946cc66a2571ff4a
Cassandra Perez <cassandraenriq@icloud.com>
2020-06-23 18:55:15 -0500

    updated files within  code

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var mysql = require ('mysql');
 var inquire = require = require ('inquire');
 var util = require ('util');
 
-var options  = {
-    user 'root',
-    password
-}
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "movie_planner_db"
 
+});
 
 var connection = mysql.createConnection(options);
 
@@ -27,9 +30,47 @@ connection.connect(function (err) {
 
 const questions = [
     {
-        name: 'choice'
+        name: 'choice',
         message:' what would you like to do:',
-        type: 'list'
-        choices: "Get all employeess", "Get all roles", "Add all departments": "Add an department"],
+        type: 'list',
+        choices: ["Get all employeess", "Get all roles", "Add all departments", "Add an department"],
     },
 ];
+
+//{
+    choice: "Get all employess"
+//}
+function startApp() {
+    inquire.promt(questions).then(answers => {
+    switch (answers.choice) {
+    case 'Get all roles':
+        inquirer.prompt(internQues).then(function(answers){
+            console.log 
+            startApp();
+        });
+
+}
+
+
+break;
+ "Get all departments":
+
+ inquire.promt(egineerQues).then(function)(answers){
+     console.log("you asked engneer questions")
+     var tableRows = await connection.query('select * from sometable');
+     startApp();
+ });
+
+ break;
+ case "add department":
+
+
+ default:
+     console.log("your done entering employees!");
+    }
+}).catch(err=>){
+    console.log(err);
+})
+
+}
+startApp();
