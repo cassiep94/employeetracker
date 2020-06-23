e02542952c67c2f3d1b8a1bcc959737a80788dde
Cassandra Perez <cassandraenriq@icloud.com>
2020-06-23 16:09:03 -0500

    index js

---
+++ index.js
@@ -0,0 +1,14 @@
+var mysql = require ('mysql');
+var in quire = require = require ('inquire');
+var util = require ('util');
+
+var options  = {
+    user 'root',
+    password
+}
+
+
+var data = connection.query
+var conncetion = sql.createConnection(options);
+
+connection.query = util.promsify(connection.query);
